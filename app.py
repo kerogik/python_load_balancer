162df4ae495f0fa1ca8057e2127efdd1e59451ce
Igor Kleschyov <idkleschyov@edu.hse.ru>
2022-10-25 18:36:35 +0300

    slight changes to webpage output

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def sample():
-    return "this is the {} service".format(os.environ["APP"])
+    return "this is the {} service at server nr. {}".format(os.environ["APP"], os.environ["SERV"])
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
